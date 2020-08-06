e30eed8f560f05b45297d85b6a47dbdbe84c1653
FackJitzgerald <34908794+FackJitzgerald@users.noreply.github.com>
2020-08-06 10:06:13 -0600

    Update goeonet.go

--- goeonet.go
+++ goeonet.go
@@ -1,6 +1,9 @@
 package goeonet
 
 import (
+  "fmt"
+  "log"
+  "io/ioutil"
   "net/http"
   "time"
 )
@@ -9,9 +12,30 @@ import (
 var client = http.Client{Timeout: 5 * time.Second}
 
 func main() {
-
+  GetRecentOpenEvents()
 }
 
 func GetRecentOpenEvents() {
-  
+  req, err := http.NewRequest("GET", "https://eonet.sci.gsfc.nasa.gov/api/v3/events", nil)
+
+  if err != nil {
+    log.Fatal("NewRequest: ", err)
+  }
+
+  req.Header.Add("status", "open")
+  req.Header.Add("limit", "20")
+
+  resp, err := client.Do(req)
+
+  if err != nil {
+    log.Fatal("Do: ", err)
+  }
+
+  responseData, err := ioutil.ReadAll(resp.Body)
+
+  if err != nil {
+    log.Fatal("ReadAll: ", err)
+  }
+
+  fmt.Println(responseData)
 }
