996ca4b94803aee9426d3e41884533abdd0db98b
FackJitzgerald <34908794+FackJitzgerald@users.noreply.github.com>
2020-08-07 01:15:34 -0600

    Added structs for the remaining values returned from the API

--- goeonet.go
+++ goeonet.go
@@ -13,19 +13,40 @@ const (
   baseUrl = "https://eonet.sci.gsfc.nasa.gov/api/v3/events"
 )
 
+type Category struct {
+  Id    string `json:"id"`
+  Title string `json:"title"`
+}
+
 type Source struct {
   Id  string `json:"id"`
   Url string `json:"url"`
 }
 
+type Geometry struct {
+  MagnitudeValue string    `json:"magnitudeValue"`
+  MagnitudeUnit  string    `json:"magnitudeUnit"`
+  Date           string    `json:"date"`
+  Type           string    `json:"type"`
+  Coordinates    []float64 `json:"coordinates"`
+}
+
 type Event struct {
-  Id      string   `json:"id"`
-  Title   string   `json:"title"`
-  Sources []Source `json:"sources"`
+  Id          string     `json:"id"`
+  Title       string     `json:"title"`
+  Description string     `json:"description"`
+  Link        string     `json:"link"`
+  Closed      string     `json:"closed"`
+  Categories  []Category `json:"categories"`
+  Sources     []Source   `json:"sources"`
+  Geometrics  []Geometry `json:"geometry"`
 }
 
 type EventCollection struct {
-  Events []Event `json:"events"`
+  Title       string  `json:"title"`
+  Description string  `json:"description"`
+  Link        string  `json:"link"`
+  Events      []Event `json:"events"`
 }
 
 var client = http.Client{Timeout: 5 * time.Second}
@@ -42,7 +63,7 @@ func main() {
     for _, source := range event.Sources {
       fmt.Printf("\tURL: %s\n", source.Url)
     }
-    fmt.Println()
+    fmt.Printf("Coordinates: %f, %f\n\n", event.Geometrics[0].Coordinates[0], event.Geometrics[0].Coordinates[1])
   }
 }
 
