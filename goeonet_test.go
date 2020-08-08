102d8b851b510ef1892792d3c7aeafcc9bc2b394
FackJitzgerald <34908794+FackJitzgerald@users.noreply.github.com>
2020-08-08 04:49:44 -0600

    Added basic test for GetEventsByDate

--- goeonet_test.go
+++ goeonet_test.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-  "fmt"
   "testing"
 )
 
 func TestGetRecentOpenEventsBasic(t *testing.T) {
   eventCollection, err := GetRecentOpenEvents(1)
 
-  if eventCollection.Title != "EONET Events" {
+  if err != nil {
+    t.Error("TestGetRecentOpenEventsBasic: ", err)
+  }
+
+  if eventCollection.Title != "EONET Events" && eventCollection.Link != baseEventsUrl {
     t.Error("TestGetRecentOpenEventsBasic: an error has likely occurred while querying the events API")
   }
 
   if len(eventCollection.Events) > 1 {
     t.Error("TestGetRecentOpenEventsBasic: number of events returned exceeded the limit")
+  }
+}
+
+func TestGetEventsByDateBasic(t *testing.T) {
+  eventCollection, err := GetEventsByDate("2010-01-01", "2020-01-01")
+
+  if err != nil {
+    t.Error("TestGetEventsByDateBasic: ", err)
+  }
+
+  if err == nil && len(eventCollection.Events) < 1 {
+    t.Error("TestGetEventsByDateBasic: there should be at least some events that occured from 2010-2020")
   }
 }
