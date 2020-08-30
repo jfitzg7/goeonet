e4a674544025a17e3f239103cb36aa93974c84c1
Jack Fitzgerald <jackf8088@gmail.com>
2020-08-30 02:07:02 -0600

    Moved sources api testing to separate file

--- goeonet_test.go
+++ goeonet_test.go
@@ -2,26 +2,6 @@ package goeonet
 
 import "testing"
 
-func TestGetSourcesBasic(t *testing.T) {
-  collection, err := GetSources()
-
-  if err != nil {
-    t.Error(err)
-  }
-
-  if collection.Title != "EONET Event Sources" {
-    t.Error("The title returned from the api doesn't match")
-  }
-
-  if collection.Link != baseSourcesUrl {
-    t.Error("The link returned from the api doesn't match")
-  }
-
-  if len(collection.Sources) < 1 {
-    t.Error("There should be at least some sources returned by the API")
-  }
-}
-
 func TestGetCategoriesBasic(t *testing.T) {
   collection, err := GetCategories()
 
