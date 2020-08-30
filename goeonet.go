4ba2e03388c864e05974575c2e029fc72d283905
Jack Fitzgerald <jackf8088@gmail.com>
2020-08-30 01:48:20 -0600

    Moved sources api functionality into separate file

--- goeonet.go
+++ goeonet.go
@@ -49,20 +49,6 @@ func (l *Layers) UnmarshalJSON(data []byte) error {
 	}
 }
 
-type Source struct {
-	Id     string `json:"id"`
-	Title  string `json:"title"`
-	Source string `json:"source"`
-	Link   string `json:"link"`
-}
-
-type Sources struct {
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	Link        string   `json:"link"`
-	Sources     []Source `json:"sources"`
-}
-
 type Collection struct {
 	Title       string     `json:"title"`
 	Description string     `json:"description"`
@@ -73,30 +59,6 @@ type Collection struct {
 }
 
 var client = http.Client{Timeout: 5 * time.Second}
-
-func GetSources() (*Collection, error) {
-	collection, err := querySourcesApi()
-	if err != nil {
-		return nil, err
-	}
-
-	return collection, nil
-}
-
-func querySourcesApi() (*Collection, error) {
-	responseData, err := sendRequest(baseSourcesUrl)
-	if err != nil {
-		return nil, err
-	}
-
-	var collection Collection
-
-	if err := json.Unmarshal(responseData, &collection); err != nil {
-		return nil, err
-	}
-
-	return &collection, nil
-}
 
 func GetLayers() (*Collection, error) {
 	collection, err := queryLayersApi(baseLayersUrl)
