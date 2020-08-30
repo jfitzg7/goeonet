248eac9903a92b5f361dda2e770c4d4f45416d94
Jack Fitzgerald <jackf8088@gmail.com>
2020-08-30 02:08:08 -0600

    Moved categories api testing to separate file

--- goeonet_test.go
+++ goeonet_test.go
@@ -2,38 +2,6 @@ package goeonet
 
 import "testing"
 
-func TestGetCategoriesBasic(t *testing.T) {
-  collection, err := GetCategories()
-
-  if err != nil {
-    t.Error(err)
-  }
-
-  if collection.Title != "EONET Event Categories" {
-    t.Error("The title returned from the api doesn't match")
-  }
-
-  if collection.Link != baseCategoriesUrl {
-    t.Error("The link returned from the api doesn't match")
-  }
-}
-
-func TestGetEventsByCategoryIDLandslides(t *testing.T) {
-  collection, err := GetEventsByCategoryID("landslides")
-
-  if err != nil {
-    t.Error(err)
-  }
-
-  if collection.Title != "EONET Events: Landslides" {
-    t.Error("The title returned from the api doesn't match")
-  }
-
-  if collection.Link != baseCategoriesUrl + "/landslides" {
-    t.Error("The link returned from the api doesn't match")
-  }
-}
-
 func TestGetLayersBasic(t *testing.T) {
   collection, err := GetLayers()
 
