82bf0947a8557aa5bb9e209dba168d0201a5d60d
Jack Fitzgerald <jackf8088@gmail.com>
2020-08-29 22:18:58 -0600

    Added GeometryType for Point and Polygon

--- geometry.go
+++ geometry.go
@@ -1,6 +1,22 @@
 package goeonet
 
-import "encoding/json"
+import (
+  "encoding/json"
+  "errors"
+)
+
+/*
+  Inspired by paulmach's go.geojson package. The EONET API
+  uses foreign members in their geometry objects, so a custom
+  implementation must be used to unmarshal the json properly.
+*/
+
+type GeometryType string
+
+const (
+  GeometryPoint   GeometryType = "Point"
+  GeometryPolygon GeometryType = "Polygon"
+)
 
 type Geometry struct {
 	MagnitudeValue float64     `json:"magnitudeValue"`
