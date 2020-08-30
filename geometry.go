636bcd0c02d42936fce918706cfaf7a1ea72f89a
Jack Fitzgerald <jackf8088@gmail.com>
2020-08-29 22:19:52 -0600

    Created function for unmarshaling JSON for the Geometry type

--- geometry.go
+++ geometry.go
@@ -26,3 +26,70 @@ type Geometry struct {
 	Point    			 []float64
 	Polygon        [][][]float64
 }
+
+func (*g Geometry) UnmarshalJSON(data []byte) error {
+  var object map[string]interface{}
+  err := json.Unmarshal(data, &object)
+  if err != nil {
+    return err
+  }
+
+  return decodeGeometry(g, object)
+}
+
+func decodeGeometry(g *Geometry, object map[string]interface{}) error {
+  mv, ok := object["magnitudeValue"]
+  if !ok {
+    return errors.New("magnitudeValue property is not defined")
+  }
+
+  if s, ok := mv.(string); ok {
+    g.MagnitudeValue = s
+  } else if f, ok := mv.(float64); ok {
+    g.MagnitudeValue = f
+  } else {
+    return errors.New("magnitudeValue property is neither string nor float64")
+  }
+
+  mu, ok := object["magnitudeUnit"]
+  if !ok {
+    return errors.New("magnitudeUnit property is not defined")
+  }
+
+  if s, ok := mv.(string); ok {
+    g.MagnitudeUnit = s
+  } else {
+    return errors.New("magnitudeUnit property is not a string")
+  }
+
+  d, ok := object["date"]
+  if !ok {
+    return errors.New("date property is not defined")
+  }
+
+  if s, ok := d.(string); ok {
+    g.Date = s
+  } else {
+    return errors.New("date property is not a string")
+  }
+
+  t, ok := object["type"]
+  if !ok {
+    return errors.New("type property is not defined")
+  }
+
+  if s, ok := t.(string); ok {
+    g.Type = GeometryType(s)
+  } else {
+    return errors.New("type property is not a string")
+  }
+
+  switch g.Type {
+  case GeometryPoint:
+    g.Point, err = decodePoint(object["coordinates"])
+  case GeometryPolygon:
+    g.Polygon, err = decodePolygon(object["coordinates"])
+  }
+
+  return nil
+}
