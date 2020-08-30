a7d97dd985cb96c9efd24c345126a3f859178747
Jack Fitzgerald <jackf8088@gmail.com>
2020-08-29 19:18:54 -0600

    Deleted Coordinates type and replaced with Point and Polygon

--- goeonet.go
+++ goeonet.go
@@ -84,29 +84,6 @@ type Sources struct {
 	Sources     []Source `json:"sources"`
 }
 
-type Coordinates struct {
-	Coordinates [][]float64
-}
-
-func (c *Coordinates) UnmarshalJSON(data []byte) error {
-	dataString := strings.Replace(string(data), " ", "", -1)
-	dataString = strings.Replace(dataString, "],", "", -1)
-	dataString = strings.Replace(dataString, "]", "", -1)
-	dataString = strings.Replace(dataString, "[[", "", -1)
-	coordinates := make([][]float64, 0)
-	for _, coords := range strings.Split(dataString[1:], "[") {
-		coordArr := make([]float64, 0)
-		split := strings.Split(coords, ",")
-		coord1, _ := strconv.ParseFloat(split[0], 64)
-		coord2, _ := strconv.ParseFloat(split[1], 64)
-		coordArr = append(coordArr, coord1)
-		coordArr = append(coordArr, coord2)
-		coordinates = append(coordinates, coordArr)
-	}
-	c.Coordinates = coordinates
-	return nil
-}
-
 type EventSource struct {
 	Id     string `json:"id"`
 	Url    string `json:"url"`
@@ -117,7 +94,8 @@ type Geometry struct {
 	MagnitudeUnit  string      `json:"magnitudeUnit"`
 	Date           string      `json:"date"`
 	Type           string      `json:"type"`
-	Coordinates    Coordinates `json:"coordinates"`
+	Point    			 []float64
+	Polygon        [][][]float64
 }
 
 type Event struct {
