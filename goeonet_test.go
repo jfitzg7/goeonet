201acaa37dd69738422a311f9207a691e4d83f5c
Jack Fitzgerald <jackf8088@gmail.com>
2020-08-30 02:05:48 -0600

    Moved events api testing to separate file

--- goeonet_test.go
+++ goeonet_test.go
@@ -1,48 +1,6 @@
 package goeonet
 
-import (
-  "testing"
-)
-
-func TestGetRecentOpenEventsBasic(t *testing.T) {
-  collection, err := GetRecentOpenEvents("1")
-
-  if err != nil {
-    t.Error(err)
-  }
-
-  if collection.Title != "EONET Events" && collection.Link != baseEventsUrl {
-    t.Error("An error has likely occurred while querying the events API")
-  }
-
-  if len(collection.Events) > 1 {
-    t.Error("Number of events returned exceeded the limit")
-  }
-}
-
-func TestGetEventsByDateBasic(t *testing.T) {
-  collection, err := GetEventsByDate("2010-01-01", "2020-01-01")
-
-  if err != nil {
-    t.Error(err)
-  }
-
-  if len(collection.Events) < 1 {
-    t.Error("There should be at least some events that occured from 2010-2020")
-  }
-}
-
-func TestGetEventsBySourceID(t *testing.T) {
-  collection, err := GetEventsBySourceID("PDC")
-
-  if err != nil {
-    t.Error(err)
-  }
-
-  if len(collection.Events) < 1 {
-    t.Error("There should be at least some events whose source is the Pacific Disaster Center (PDC)")
-  }
-}
+import "testing"
 
 func TestGetSourcesBasic(t *testing.T) {
   collection, err := GetSources()
