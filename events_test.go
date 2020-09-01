620874aba9a4eb7310101fd6dd8973b528347eca
Jack Fitzgerald <jackf8088@gmail.com>
2020-09-01 02:47:57 -0600

    Added test for checking that the correct url is used
    For hitting the control flow that isn't hit in other tests when constructing an events api query

--- events_test.go
+++ events_test.go
@@ -28,19 +28,35 @@ func TestGetEventsWithLimit(t *testing.T) {
   client = &http.Client{Timeout: 5 * time.Second}
 }
 
-func TestGetEventsWithSource(t *testing.T) {
+func TestGetEventsForCorrectUrl(t *testing.T) {
   mockCtrl := gomock.NewController(t)
 
   mockHTTPClient := mocks.NewMockHTTPClient(mockCtrl)
   client = mockHTTPClient
 
-  request, _ := http.NewRequest("GET", "https://eonet.sci.gsfc.nasa.gov/api/v3/events?limit=1&source=InciWeb", nil)
+  url := "https://eonet.sci.gsfc.nasa.gov/api/v3/events?bbox=-129.02%2C50.73%2C-58.71%2C12.89&days=20&end=2019-01-31&magID=mag_kts&magMax=20&magMin=1.50&start=2019-01-01"
 
-  response := &http.Response{StatusCode: 200, Body: ioutil.NopCloser(bytes.NewReader([]byte("")))}
+  request, err := http.NewRequest("GET", url, nil)
+
+  if err != nil {
+    t.Error(err)
+  }
+
+  response := &http.Response{Body: ioutil.NopCloser(bytes.NewReader([]byte("")))}
 
   mockHTTPClient.EXPECT().Do(gomock.Eq(request)).Return(response, nil).Times(1)
 
-  GetEvents(EventsQueryParameters{Source: "InciWeb", Limit: 1})
+  query := EventsQueryParameters {
+    Days: 20,
+    Start: "2019-01-01",
+    End: "2019-01-31",
+    MagID: "mag_kts",
+    MagMin: "1.50",
+    MagMax: "20",
+    Bbox: "-129.02,50.73,-58.71,12.89",
+  }
+
+  GetEvents(query)
 
   client = &http.Client{Timeout: 5 * time.Second}
 }
