220f760db138d9691eb1821434d1a414ca4ef572
Jack Fitzgerald <jackf8088@gmail.com>
2020-09-01 00:46:08 -0600

    Added a second mock test and removed the old ones

--- events_test.go
+++ events_test.go
@@ -11,7 +11,7 @@ import (
   "github.com/jfitzg7/goeonet/mocks"
 )
 
-func TestGetEvents(t *testing.T) {
+func TestGetEventsWithLimit(t *testing.T) {
   mockCtrl := gomock.NewController(t)
 
   mockHTTPClient := mocks.NewMockHTTPClient(mockCtrl)
@@ -28,60 +28,19 @@ func TestGetEvents(t *testing.T) {
   client = &http.Client{Timeout: 5 * time.Second}
 }
 
-func TestGetRecentOpenEvents(t *testing.T) {
-  collection, err := GetEvents(EventsQueryParameters{Status: "open", Limit: 1})
+func TestGetEventsWithSource(t *testing.T) {
+  mockCtrl := gomock.NewController(t)
 
-  if err != nil {
-    t.Error(err)
-  }
+  mockHTTPClient := mocks.NewMockHTTPClient(mockCtrl)
+  client = mockHTTPClient
 
-  if collection.Title != "EONET Events" && collection.Link != baseEventsUrl {
-    t.Error("An error has likely occurred while querying the events API")
-  }
+  request, _ := http.NewRequest("GET", "https://eonet.sci.gsfc.nasa.gov/api/v3/events?limit=1&source=InciWeb", nil)
 
-  // There might be 0 recent open events, so checking for != 1 won't work
-  if len(collection.Events) > 1 {
-    t.Error("Number of open events returned exceeded the limit")
-  }
-}
+  response := &http.Response{StatusCode: 200, Body: ioutil.NopCloser(bytes.NewReader([]byte("")))}
 
-func TestGetRecentClosedEvents(t *testing.T) {
-  collection, err := GetEvents(EventsQueryParameters{Status: "closed", Limit: 1})
+  mockHTTPClient.EXPECT().Do(gomock.Eq(request)).Return(response, nil).Times(1)
 
-  if err != nil {
-    t.Error(err)
-  }
+  GetEvents(EventsQueryParameters{Source: "InciWeb", Limit: 1})
 
-  if collection.Title != "EONET Events" && collection.Link != baseEventsUrl {
-    t.Error("An error has likely occurred while querying the events API")
-  }
-
-  // There should always be at least 1 closed event returned
-  if len(collection.Events) != 1 {
-    t.Error("Number of closed events returned does not match the specified limit")
-  }
-}
-
-func TestGetEventsByDateBasic(t *testing.T) {
-  collection, err := GetEvents(EventsQueryParameters{Start: "2010-01-01", End: "2020-01-01"})
-
-  if err != nil {
-    t.Error(err)
-  }
-
-  if len(collection.Events) < 1 {
-    t.Error("There should be at least some events that occured from 2010-2020")
-  }
-}
-
-func TestGetEventsBySourceID(t *testing.T) {
-  collection, err := GetEvents(EventsQueryParameters{Source: "PDC"})
-
-  if err != nil {
-    t.Error(err)
-  }
-
-  if len(collection.Events) < 1 {
-    t.Error("There should be at least some events whose source is the Pacific Disaster Center (PDC)")
-  }
+  client = &http.Client{Timeout: 5 * time.Second}
 }
