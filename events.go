d9c8a4f5c7f234fe0b252401b8a3f8556b49c510
Jack Fitzgerald <jackf8088@gmail.com>
2020-09-12 10:50:24 -0600

    Added function for querying geojson events api

--- events.go
+++ events.go
@@ -5,7 +5,10 @@ import (
 	"net/url"
 )
 
-const baseEventsUrl = "https://eonet.sci.gsfc.nasa.gov/api/v3/events"
+const (
+	baseEventsUrl = "https://eonet.sci.gsfc.nasa.gov/api/v3/events"
+	baseGeoJsonEventsUrl = "https://eonet.sci.gsfc.nasa.gov/api/v3/events/geojson"
+)
 
 // EventsQueryParameters is used for specifying the query parameters
 // that can be passed to the GetEvents function. More information on
@@ -40,6 +43,59 @@ func createEventsApiUrl(query EventsQueryParameters) url.URL {
 		Scheme: "https",
 		Host:   "eonet.sci.gsfc.nasa.gov",
 		Path:   "/api/v3/events",
+	}
+	q := u.Query()
+	if query.Source != "" {
+		q.Set("source", query.Source)
+	}
+	if query.Status != "" {
+		q.Set("status", query.Status)
+	}
+	if query.Limit > 0 {
+		q.Set("limit", fmt.Sprint(query.Limit))
+	}
+	if query.Days > 0 {
+		q.Set("days", fmt.Sprint(query.Days))
+	}
+	if query.Start != "" {
+		q.Set("start", query.Start)
+		if query.End != "" {
+			q.Set("end", query.End)
+		}
+	}
+	if query.MagID != "" {
+		q.Set("magID", query.MagID)
+	}
+	if query.MagMin != "" {
+		q.Set("magMin", query.MagMin)
+	}
+	if query.MagMax != "" {
+		q.Set("magMax", query.MagMax)
+	}
+	if query.Bbox != "" {
+		q.Set("bbox", query.Bbox)
+	}
+	u.RawQuery = q.Encode()
+	return u
+}
+
+// GetGeoJsonEvents gets a list of events in GeoJSON format based on the query parameters passed in
+func GetGeoJsonEvents(query EventsQueryParameters) ([]byte, error) {
+	url := createGeoJsonEventsApiUrl(query)
+
+	responseData, err := sendRequestToEonetApi(url.String())
+	if err != nil {
+		return nil, err
+	}
+
+	return responseData, nil
+}
+
+func createGeoJsonEventsApiUrl(query EventsQueryParameters) url.URL {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "eonet.sci.gsfc.nasa.gov",
+		Path:   "/api/v3/events/geojson",
 	}
 	q := u.Query()
 	if query.Source != "" {
